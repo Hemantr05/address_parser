ff60a5fd7e46aec77746d24127ccfb4a65654173
Hemant Rakesh <basketballlfe600@gmail.com>
2021-09-27 03:05:36 +0530

    added testcases

--- tests/test_address_parser.py
+++ tests/test_address_parser.py
@@ -1,5 +1,16 @@
 from address_parser import __version__
-
+from address_parser.parse import extract
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def find_zipcode():
+    text = "This is a zipcode 560094 for RMV II tage, Bangalore"
+    pincode = "560094"
+    area = "Rmv II stage"
+    city = "Bangalore"
+    
+    extracted_addr = extract(text)
+    assert extracted_addr['Zipcode'] == pincode
+    assert extracted_addr['Area'] == area
+    assert extracted_addr['City'] == city
