006d6d1d88e1462d999b0b958e51b1df6f4dc695
Hemant Rakesh <basketballlfe600@gmail.com>
2021-09-27 02:53:34 +0530

    found zipcode, area, city and state

--- address_parser/parse.py
+++ address_parser/parse.py
@@ -1,25 +1,58 @@
 import pandas as pd
+from . import utils
 
-from utils import *
+dataset = pd.read_csv('address_parser/all_india_pin_code.csv', encoding='iso-8859-1')
 
-def find(str: entity, str: text_blob):
-    """Find entity such as city, zipcode, state, etc.
-       from block of text
-    """
-    pass
+def find_pincode(text):
+	"""Best match for pincode"""
+	pincode = None
+	text = text.split(" ")
+	for idx in range(len(text)-1, 0, -1):
+		if len(text[idx]) == 6:
+			try:
+				code = int(text[idx].strip())
+				pincode = code
+				break
+			except:
+				continue
+		continue
+	return pincode
 
-def extract(str: text_blob):
-    """Extract individual address entities (zipcode, city, etc)
-       from a blob of text
-    """
-    entities = {
-		"State": None,
-		"City": None,
-		"Zipcode": None,
-		"Town": None,
-		"Area": None,
-		"HouseNo": None,
-		"Landmark": None,
-		"Building/Apartment": None,
-		}
-    pass
+def find(text, candidates):
+	"""Find entities based on similarity"""
+
+	result = utils.token_similarity(text, candidates)
+	if result:
+		value, score, index = result[0]
+		return value.capitalize()
+	return None
+	
+def extract(text_blob):
+	"""Extract individual address entities (zipcode, city, etc)
+		from a blob of text
+	"""
+	entities = {
+				"Building/Apartment": None,
+				"HouseNo": None,
+				"Area": None,
+				"Landmark": None,
+				"Town": None,
+				"City": None,
+				"State": None,
+				"Zipcode": None,
+				}
+	text_blob = utils.clean_text(text_blob)
+	pincode = find_pincode(text_blob)
+	values = dataset.loc[dataset['pincode']==pincode]
+	office_type = [utils.clean_text(name) for name in values["officeType"]]
+	office_name = [utils.clean_text(name) for name in values["officename"]]
+	city_name = [utils.clean_text(name) for name in values["Districtname"]]
+	state_name = [utils.clean_text(name) for name in values["statename"]]
+	area = find(text_blob, office_name)
+	city = find(text_blob, city_name)
+	state = find(text_blob, state_name)
+	entities["Zipcode"] = pincode
+	entities["Area"] = area
+	entities["City"] = city
+	entities["State"] = state
+	return entities
